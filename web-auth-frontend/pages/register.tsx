bba03cfe9feb91642fa47ca49d203a71a9165572
Tyna William <tynawilliam@gmail.com>
2023-07-25 11:26:34 -0400

    registration complete

--- web-auth-frontend/pages/register.tsx
+++ web-auth-frontend/pages/register.tsx
@@ -1,13 +1,12 @@
 import "./globals.css";
 import RegisterForm from "@/components/register-form";
-import { client } from '@passwordless-id/webauthn';
+import { client } from "@passwordless-id/webauthn";
 
 /**
  * @return {JSX.Element} The JSX element
  */
 function RegisterPage() {
-
-  async function handleRegister(event:React.FormEvent<HTMLFormElement>) {
+  async function handleRegister(event: React.FormEvent<HTMLFormElement>) {
     event.preventDefault();
     console.log("haha I was clickedd");
 
@@ -18,39 +17,34 @@ function RegisterPage() {
 
     const challenge = "a7c61ef9-dc23-4806-b486-2428938a547e";
     const registration = await client.register("David", challenge, {
-        "authenticatorType": "auto",
-        "userVerification": "required",
-        "timeout": 60000,
-        "attestation": false,
-        "debug": false
-      });
+      authenticatorType: "auto",
+      userVerification: "required",
+      timeout: 60000,
+      attestation: false,
+      debug: false
+    });
 
-      console.log("I am reg",registration)
+    console.log("I am reg", registration);
 
+    const body = { email: formDataAsObject.email, registration: registration };
 
-      const body = { email: formDataAsObject.email, registration : registration };
-
-      console.log("ahhaha body ",body)
+    console.log("ahhaha body ", body);
 
     fetch("http://localhost:5001/api/auth/register", {
-        method: "POST",
-        mode: "cors",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(body),
+      method: "POST",
+      mode: "cors",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(body)
     })
-    .then((response) => response.json())
-    .then((jsonResponse)=>{
+      .then((response) => response.json())
+      .then((jsonResponse) => {
         console.log(jsonResponse);
-        console.log("I am status", jsonResponse.status);
         // router.push("/about");
-    })
-    .catch((error) => console.log(error));
-}
-
-
+      })
+      .catch((error) => console.log(error));
+  }
 
   return <RegisterForm handleRegister={handleRegister} />;
 }
-
 
 export default RegisterPage;
